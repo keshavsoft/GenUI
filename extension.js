f8fe4bef15b21d663e479cf49f1e4f6bf1390da6
keshavsoft <nknnkumar@yahoo.com>
2026-03-02 18:46:54 +0530

    v 1.1.2

--- extension.js
+++ extension.js
@@ -1,11 +1,11 @@
 // extension.js
 const vscode = require('vscode');
 
-const { StartFunc: StartFuncV24 } = require('./src/V24/entryFile');
+const { StartFunc: StartFuncV1 } = require('./src/V1/entryFile');
 
 const activate = async (context) => {
     console.log('Congratulations, your extension "create-folder" is now active!');
-    StartFuncV24();
+    StartFuncV1();
 
     vscode.commands.registerCommand("extension.Path", () => {
         vscode.window.showInformationMessage(__dirname);
---
+++ src/V1/entryFile.js
@@ -0,0 +1,45 @@
+const vscode = require('vscode');
+const fse = require('fs-extra');
+const path = require('path');
+
+const CommonRegisterCommand = "NonSecured";
+
+const { StartFunc: StartFuncrunNodeApp } = require("./ServerRun");
+
+// pull the columns schema from the json file referred from schema.json
+
+const StartFunc = () => {
+    vscode.commands.registerCommand(CommonRegisterCommand, LocalFuncToActivate);
+};
+
+const LocalFuncToActivate = async () => {
+    const LocalToPath = LocalFuncGetWorkSpaceFolder();
+    LocalFuncCheckFolder({ inToPath: LocalToPath });
+
+    StartFuncrunNodeApp(LocalToPath)
+};
+
+const LocalFuncCheckFolder = ({ inToPath }) => {
+    const LocalToPath = `${inToPath}\\ForFrontEnd`;
+
+    if (fse.existsSync(LocalToPath) && fse.lstatSync(LocalToPath).isDirectory()) {
+        console.log('Folder exists!');
+        return false;
+    };
+
+    const LocalFromTablePath = path.join(__dirname, "CopyCode");
+
+    fse.copySync(`${LocalFromTablePath}/ForFrontEnd`, LocalToPath);
+};
+
+const LocalFuncGetWorkSpaceFolder = () => {
+    if (vscode.workspace.workspaceFolders) {
+        const rootUri = vscode.workspace.workspaceFolders[0].uri;
+        const rootPath = rootUri.fsPath; // Get the file path
+        return rootPath;
+    } else {
+        console.log("No workspace folders found.");
+    };
+};
+
+module.exports = { StartFunc };
